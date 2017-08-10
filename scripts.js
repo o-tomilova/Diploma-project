e796ab65b8289211295b0c4e3d76bb98dbd9c96e
OlgaTom <oliatomilova@gmail.com>
2017-08-10 14:49:07 +0300

    almost done

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 
-window.onload = function() {
+window.addEventListener("load", init, false);
 	var tableContainer = document.getElementById("tableContainer"),
 		addBtn = document.getElementById("addProduct"),
 		addBlock = document.getElementById("addBlock"),
@@ -7,66 +7,84 @@ window.onload = function() {
 		submitForm = document.getElementById("submit"),
 		cancelForm = document.getElementById("cancel");
 
-// -------------------------------products array---------------------
+// ------------------- default products array -------------------
 
 	var products = [
-			{id: "1", name: "T-shirt", category: "clothes", description: "Cool summer T-shirt", price: "16.99"}, 
-			{id: "2", name: "Shorts", category: "clothes", description: "Cool summer shorts", price: "20"}, 
-			{id: "3", name: "Hawians", category: "shoes", description: "Nice beach flip-flops", price: "15"},
-			{id: "4", name: "Nacklace", category: "accessories", description: "Silver nacklace", price: "25"}, 
-			{id: "5", name: "Dress", category: "clothes", description: "Long sleeveless", price: "29.90"}, 
-			{id: "6", name: "Boots", category: "shoes", description: "Warm winter boots", price: "50"}];
-	console.log(products);
+			{id: 1, name: "T-shirt", category: "clothes", description: "Cool summer T-shirt", price: "16.99"}, 
+			{id: 2, name: "Shorts", category: "clothes", description: "Cool summer shorts", price: "20"}, 
+			{id: 3, name: "Hawians", category: "shoes", description: "Nice beach flip-flops", price: "15"},
+			{id: 4, name: "Nacklace", category: "accessories", description: "Silver nacklace", price: "25"}, 
+			{id: 5, name: "Dress", category: "clothes", description: "Long sleeveless", price: "29.90"}, 
+			{id: 6, name: "Boots", category: "shoes", description: "Warm winter boots", price: "50"}];
 
-// --------------------------adding a product ?????------------------
 
-	output = function(arr){ 
+	var output = function(){ 
 
-		var myTable = "<div><table id='productsTable' class='table'><thead align='left'>";
-		myTable += "<th>ID</th> <th>Product name</th> <th>Category</th> <th>Description</th> <th id='priceTH' data-type='number'>Price $</th> </thead>";
+		var myTable = "<table id='productsTable' class='table'><thead align='left'>";
+		myTable += "<th>ID</th> <th>Product name</th> <th>Category</th> <th>Description</th> <th id='priceTH' data-type='number'>Price $</th><th> Action </th> </thead>";
 		myTable += "<tbody>"
-			for (var i = 0; i < arr.length; i++) {
-				myTable += "<tr class="+arr[i].category+" id="+arr[i].id+"><td>" + arr[i].id + "</td> <td>" + arr[i].name + "</td><td>" + arr[i].category + "</td><td>"
-				+ arr[i].description + "</td><td class='price'>" + arr[i].price + "</td></tr>";
+			for (var i = 0; i < products.length; i++) {
+				myTable += "<tr class="+products[i].category+" id="+products[i].id+"><td>" + products[i].id + "</td> <td>" + products[i].name + "</td><td>" + products[i].category + "</td><td>"
+				+ products[i].description + "</td><td class='price'>" + products[i].price + "</td> <td class='delProduct'><button class='btn btn-danger btn-xs'>Delete</button></td></tr>";
 			}
-		myTable += "</tbody> </table> </div>";
+		myTable += "</tbody> </table>";
 		tableContainer.innerHTML = myTable;
-	}		
-	output(products); // Виводить товари з масиву, АЛЕ не додані вручну
+	}
+	output();
+
+// -------------------------- adding a product -------------------
+
+	addBtn.addEventListener("click", addingNewProd);
 
 
+	function addingNewProd () {
+		if (addBlock.className == "hideBlock") {
+			addBlock.className = "showBlock";
+		} else {
+			addBlock.className = "hideBlock"
+		}
+	};
 
-	addBtn.addEventListener("click", function(){
-		addBlock.className = "showBlock";
-	});
-
-	// function Product(id, name, category, description, price){
-	// 	this.id = id;
-	// 	this.name = name;
-	// 	this.category = category;
-	// 	this.description = description;
-	// 	this.price = price;
-	// }
 
 	var item;
 
 	submitForm.addEventListener("click", function () {
-		// var last = products.length;
-		// var item = new Product(last, document['addForm']['name'].value, document['addForm']['category'].value, document['addForm']['desc'].value, document['addForm']['price'].value)
+		var last = products.length;
+		item = {
+			id: last+1, name: document['addForm']['name'].value, 
+			category: document['addForm']['category'].value, 
+			description: document['addForm']['desc'].value, 
+			price: document['addForm']['price'].value
+		};
 
-		// products.push(item);
-		// console.log(products[last]); // виводить останній доданий елемент
-		// console.log(products);      // виводить всі товари включно з доданим
-		// output(products); // товар НЕ відображається в табоиці  
+
+		products.push(item);
+		console.log(products);
+		output();
+		document.forms["addForm"].reset();
+		addingNewProd();
 	});
+	
 
 	cancelForm.addEventListener("click", function(){
 		alert("Do you want to discard changes?");
 		document.forms["addForm"].reset();
+		addingNewProd();
 	});
 
+// ------------------------- deleting a product -----------------------
+	
+	var delProducts = document.getElementsByClassName("delProduct");
 
-// ---------------------------category sort-----------------------------
+	var delProductsArr = [].slice.call(delProducts);
+
+	for (var i = 0; i < delProductsArr.length; i++) {
+		delProductsArr[i].onclick = function(){
+			this.parentNode.parentNode.removeChild(this.parentNode);
+		}
+	}
+
+// --------------------------- category sort -----------------------------
 
 	var categorySort = document.getElementById("categorySort");
 
@@ -76,8 +94,6 @@ window.onload = function() {
 		clothesArr = document.getElementsByClassName("clothes"),
 		accessArr = document.getElementsByClassName("accessories"),
 		bagsArr = document.getElementsByClassName("bags");
-
-		// var all = [].slice.call(allll); // converting an HTML Collection into Array
 
 	function setCategory(){
 		var categoryName = categorySort.value;
@@ -165,7 +181,7 @@ window.onload = function() {
 		}
 	}
 					
-// --------------------------price sort---------------------------
+// -------------------------- price sort ---------------------------
 
 	var priceSort = document.getElementById("priceSort");
 	var productsTable = document.getElementById("productsTable");
@@ -193,13 +209,10 @@ window.onload = function() {
           return rowB.cells[colNum].innerHTML - rowA.cells[colNum].innerHTML;
         };
     	break;
-
-    		// !!!!! after output(products) sortByPrice stops working ???
-
-    	case 'defaultPriceSort':
-    	compare = function(){
-    //		output(products);  
-    	};
+    	case 'defaultPrice':
+    	compare = function(rowA, rowB){
+          return rowA.cells[0].innerHTML - rowB.cells[0].innerHTML;
+        };
     	break;
     }
     
@@ -212,9 +225,58 @@ window.onload = function() {
     }
     
     productsTable.appendChild(tbody);
-
   }
 
+// ----------------------- form validation -------------------
+	
+	function init() {
+		addForm.name.onchange = nameOnChange;
+		addForm.price.onchange = priceOnChange;
+		addForm.add.onclick = onAdd; 
+	}
 
-} //window.onload
+	function validate(elem, pattern){
+		var res = elem.value.search(pattern);
+		if (res == -1) {
+			elem.className = "error";
+		} else {
+			elem.className = "success"; 
+		} 
+		if (addForm.name.className == "success" && addForm.price.className == "success") {
+			submitForm.removeAttribute("disabled");
+		}
+	}
+
+	function nameOnChange() {
+		var pattern = /\D/;
+		validate(this, pattern);
+	}
+
+		function priceOnChange() {
+		var pattern = /\d/;
+		validate(this, pattern);
+	}
+
+	function onAdd (){
+		var invalid = false;
+
+		for (var i = 0; i < addForm.elements.length; ++i) {
+			var e = addForm.elements[i];
+			if (e.type == "text" && e.onchange) {
+				e.onchange();
+			 	if (e.className == "error") {
+					invalid = true;
+				}
+			}
+			if (invalid) {
+				return false;
+			}
+		}
+	}
+
+
+
+
+
+
 
